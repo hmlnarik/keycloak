f6869521309d69890f5d8d81cab1380b1cbc3478
Hynek Mlnarik <hmlnarik@redhat.com>
2022-11-09 16:30:04 +0100

    Test for members listing

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/federation/ldap/LDAPHardcodedGroupMapperTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/federation/ldap/LDAPHardcodedGroupMapperTest.java
@@ -16,20 +16,29 @@
  */
 package org.keycloak.testsuite.federation.ldap;
 
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.keycloak.models.GroupModel;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.RoleModel;
-import org.keycloak.models.UserModel;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.GroupRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.keycloak.testsuite.util.LDAPRule;
 
 import java.io.Serializable;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
 
 /**
  * @author sventorben
@@ -54,35 +63,74 @@ public class LDAPHardcodedGroupMapperTest extends AbstractLDAPTest implements Se
      */
     @Test
     public void testCompositeGroups() {
+        // check users
+        UserRepresentation john = testRealm().users().searchByUsername("johnkeycloak", Boolean.TRUE).stream().findAny().orElse(null);
+        assertThat(john, notNullValue());
+        final String johnId = john.getId();
+        UserResource johnRes = testRealm().users().get(johnId);
+
+        ClientRepresentation adminCli = testRealm().clients().findByClientId("admin-cli").stream().findAny().orElse(null);
+        assertThat(adminCli, notNullValue());
+
+        // check roles
+        List<RoleRepresentation> roles = testRealm().clients().get(adminCli.getId()).roles().list("client_role", false);
+        RoleRepresentation clientRoleGrantedViaHardcodedGroupMembership = roles.stream().findAny().orElse(null);
+        assertThat(clientRoleGrantedViaHardcodedGroupMembership, notNullValue());
+
+        // check groups
+        final List<GroupRepresentation> groups = testRealm().groups().groups();
+        GroupRepresentation hardcodedGroup = findGroupByName(groups, "hardcoded_group");
+        assertThat(hardcodedGroup, notNullValue());
+        GroupRepresentation parentGroup = findGroupByName(groups, "parent_group");
+        assertThat(parentGroup, notNullValue());
+        final String parentGroupId = parentGroup.getId();
+        final String hardcodedGroupId = hardcodedGroup.getId();
+        assertThat(idsOfGroups(parentGroup.getSubGroups()), hasItem(hardcodedGroupId));
+
+        // check group membership
+        assertThat(idsOfGroups(johnRes.groups()), containsInAnyOrder(hardcodedGroupId));
         testingClient.server().run(session -> {
             LDAPTestContext ctx = LDAPTestContext.init(session);
             RealmModel appRealm = ctx.getRealm();
+            UserModel user = session.users().getUserById(appRealm, johnId);
 
-            // check users
-            UserModel john = session.users().getUserByUsername(appRealm, "johnkeycloak");
-            assertThat(john, notNullValue());
-
-            // check roles
-            RoleModel clientRoleGrantedViaHardcodedGroupMembership = appRealm.getClientByClientId("admin-cli").getRole(
-                    "client_role");
-            assertThat(clientRoleGrantedViaHardcodedGroupMembership, notNullValue());
-
-            // check groups
-            GroupModel hardcodedGroup = appRealm.getGroupsStream()
-                    .filter(it -> it.getName().equals("hardcoded_group")).findFirst().orElse(null);
-            assertThat(hardcodedGroup, notNullValue());
-            GroupModel parentGroup = appRealm.getGroupsStream()
-                    .filter(it -> it.getName().equals("parent_group")).findFirst().orElse(null);
-            assertThat(parentGroup, notNullValue());
-            assertThat(hardcodedGroup.getParent(), equalTo(parentGroup));
-
-            // check group membership
-            assertThat(john.isMemberOf(hardcodedGroup), is(true));
-            assertThat(john.isMemberOf(parentGroup), is(true));
-
-            // check role membership
-            assertThat(john.hasRole(clientRoleGrantedViaHardcodedGroupMembership), is(true));
+            assertThat(user.isMemberOf(session.groups().getGroupById(appRealm, hardcodedGroupId)), is(true));
+            assertThat(user.isMemberOf(session.groups().getGroupById(appRealm, parentGroupId)), is(true));
         });
+
+        // check role membership
+        assertThat(idsOfRoles(johnRes.roles().clientLevel(adminCli.getId()).listEffective()), containsInAnyOrder(clientRoleGrantedViaHardcodedGroupMembership.getId()));
+
+        assertThat(idsOfUsers(testRealm().groups().group(hardcodedGroupId).members()), contains(johnId));
+    }
+
+    private static Set<String> idsOfGroups(List<GroupRepresentation> groups) {
+        return groups.stream().filter(Objects::nonNull).map(GroupRepresentation::getId).collect(Collectors.toSet());
+    }
+
+    private static Set<String> idsOfRoles(List<RoleRepresentation> groups) {
+        return groups.stream().filter(Objects::nonNull).map(RoleRepresentation::getId).collect(Collectors.toSet());
+    }
+
+    private static Set<String> idsOfUsers(List<UserRepresentation> groups) {
+        return groups.stream().filter(Objects::nonNull).map(UserRepresentation::getId).collect(Collectors.toSet());
+    }
+
+    private static GroupRepresentation findGroupByName(List<GroupRepresentation> groups, String name) {
+        if (groups == null) {
+            return null;
+        }
+        return groups.stream()
+          .filter(g -> Objects.equals(name, g.getName()))
+          .findAny().orElseGet(() -> {
+              for (GroupRepresentation group : groups) {
+                  GroupRepresentation subgroup = findGroupByName(group.getSubGroups(), name);
+                  if (subgroup != null) {
+                      return subgroup;
+                  }
+              }
+              return null;
+          });
     }
 
 }
